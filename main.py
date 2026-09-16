ddf2b05cb94926160f891d5e87de85e089f9ca62
Humiditron <223341941+Humiditron@users.noreply.github.com>
2026-09-15 20:24:28 -0400

    Update main.py
    - changed revision number.

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ from pywebpush import webpush, WebPushException
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("webpush-relay")
 
-APP_VERSION = "1.0.3"
+APP_VERSION = "1.0.4"
 START_TIME = datetime.now(timezone.utc)
 
 # Diagnostic telemetry state
